fe3ac984e96723c56b856ed58834f7ea7032442f
saran9f98-oss <saranya9nayak@gmail.com>
2025-05-16 15:05:21 +0530

    created db

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template , request
 import os
+from model import *
 
-app = Flask(__name__)
 
-@app.route("/", methods=["GET"]) # NOTE: / =  http://192.168.1.42:5000
+
+current_dir = os.path.abspath(os.path.dirname(__file__)) 
+
+app= Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(current_dir, "Database.sqlite3")
+
+db.init_app(app)
+app.app_context().push()
+
+@app.route("/", methods=["GET", "POST"]) # NOTE: / =  http://192.168.1.42:5000
 def login():
+    if request.method == "POST":
+        user_email = request.form['email']
+        user_password = request.form['password']
+        print(user_email)
+        print(user_password)
+        return render_template("home.html", message=user_email)
     return render_template("login.html")
 
 @app.route("/user_signup", methods=["GET"])
@@ -12,5 +27,6 @@ def signup():
     return render_template("signup.html")
 
 if __name__ == "__main__":
+    db.create_all()
     app.debug = True
     app.run(host="0.0.0.0")
