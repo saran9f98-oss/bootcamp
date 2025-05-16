e5d42feffb6b83d2a34000075d5ebd6398ae0544
saran9f98-oss <saranya9nayak@gmail.com>
2025-05-16 14:14:46 +0530

    intro to templates

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 import os
 
 app = Flask(__name__)
 
 @app.route("/", methods=["GET"]) # NOTE: / =  http://192.168.1.42:5000
-def home():
-    return "Hello, World!"
+def login():
+    return render_template("login.html")
+
+@app.route("/user_signup", methods=["GET"])
+def signup():
+    return render_template("signup.html")
 
 if __name__ == "__main__":
+    app.debug = True
     app.run(host="0.0.0.0")
