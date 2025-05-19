4672812ae8dbd3ad9b39fa00e461d0eb6d6bdd7d
saran9f98-oss <saranya9nayak@gmail.com>
2025-05-19 14:04:28 +0530

    db drowser

--- model.py
+++ model.py
@@ -2,9 +2,26 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class User(db.Model):
-    __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)  
-    password = db.Column(db.String(120), nullable=False)
+class users(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(), nullable=False)
+    email = db.Column(db.String(), unique=True, nullable=False)
+    password = db.Column(db.String(), nullable=False)
+    user_type = db.Column(db.String(), nullable=False)
+
+class booking_data(db.Model):
+    __tablename__ = "booking_data"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user_id = db.Column(db.Integer)
+    resturant_id = db.Column(db.Integer)
+    date = db.Column(db.String())
+    table_id = db.Column(db.String())
+
+class resturant_data(db.Model):
+    __tablename__ = "resturant_data"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(), nullable=False)
+    location = db.Column(db.String(), nullable=False)
+    table_id = db.Column(db.Integer, nullable=False)
+   
