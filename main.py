c8e92861aa1b7aa0fd8e8fb123af30c3390afa09
saran9f98-oss <saranya9nayak@gmail.com>
2025-05-23 14:09:55 +0530

    form validation

--- main.py
+++ main.py
@@ -17,8 +17,16 @@ def login():
     if request.method == "POST":
         user_email = request.form['email']
         user_password = request.form['password']
-        user = users.query.filter_by(email=user_email, password=user_password).first()
-        return render_template("home.html", username=user.name)
+        user = users.query.filter_by(email=user_email).first()
+
+        if user is None:
+            return 'User not found'
+        else:
+            if user.password != user_password:
+                return 'Incorrect password'
+            else:
+                return render_template("home.html", username=user.name)
+            
     return render_template("login.html")
 
 @app.route("/user_signup", methods=["GET", "POST"])
@@ -29,9 +37,15 @@ def signup():
         user_password = request.form['password']
         user_type = request.form['user_type']
 
-        new_user = users(name=user_name, email=user_email, password=user_password, user_type=user_type)
-        db.session.add(new_user)
-        db.session.commit()
+        if users.query.filter_by(email=user_email).first():
+            return 'Email already exists'
+        else:
+            if user_type not in ['admin', 'customer', 'resturant']:
+                return 'Invalid user type'
+            else:
+                new_user = users(name=user_name, email=user_email, password=user_password, user_type=user_type)
+                db.session.add(new_user)
+                db.session.commit()
 
         return redirect(url_for("login"))
     return render_template("signup.html")
